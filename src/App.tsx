5b2d31e13f2d1c7d05ea784cf048af0854396096
ioddgamers <toluloperbrts@gmail.com>
2024-07-09 17:34:17 +0100

    Created new component for Result section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ResultSection from "./Components/ResultSection";
 import Test from "./Components/Test";
 
 function App() {
   return (
     <div>
       <Test />
+      <ResultSection />
     </div>
   );
 }
---
+++ src/Components/ResultSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ResultSection() {
+  return (
+    <div>
+      <p>Your Result</p>
+      <h1>76</h1>
+      <p>of 100</p>
+      <h2>Great</h2>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </div>
+  );
+}
+
+export default ResultSection;
