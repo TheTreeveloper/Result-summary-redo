bffa39f8a75260277979752ee779543663f714bd
ioddgamers <toluloperbrts@gmail.com>
2024-07-09 17:30:28 +0100

    imported assets

---
+++ src/Components/Test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Test = () => {
+  const scoreList = [
+    { id: 1, score: 80 },
+    { id: 2, score: 92 },
+    { id: 3, score: 61 },
+    { id: 4, score: 72 },
+  ];
+
+  function renderScores() {
+    return scoreList.map((score) => (
+      <div key={score.id}>
+        <p>
+          <span className="score">{score.score}</span>
+          <span className="total">/100</span>
+        </p>
+      </div>
+    ));
+  }
+
+  return (
+    <div>
+      <h1>This is a Test component.</h1>
+      <div>{renderScores()}</div>
+    </div>
+  );
+};
+
+export default Test;
