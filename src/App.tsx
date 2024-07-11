e0dfdb538d23b94c6b458bcaaa78e0ef9aa5ea74
ioddgamers <toluloperbrts@gmail.com>
2024-07-11 18:40:22 +0100

    updated App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import ResultSection from "./Components/ResultSection";
 import SummarySection from "./Components/SummarySection";
-import Test from "./Components/Test";
-import styles from "./App.css";
 
 function App() {
   return (
