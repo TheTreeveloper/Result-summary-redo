7a45c5805327cad17b45679f9ba05ede910fdade
ioddgamers <toluloperbrts@gmail.com>
2024-07-12 13:35:34 +0100

    100% completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ResultSection from "./Components/ResultSection";
 import SummarySection from "./Components/SummarySection";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ResultSection />
       <SummarySection />
     </div>
